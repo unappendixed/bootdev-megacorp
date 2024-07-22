29c22951957483d570f13841a1a097525ea4f054
Unappendixed <38593737+unappendixed@users.noreply.github.com>
2024-07-22 14:11:35 -0700

    K: add scanner script

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -1,1 +1,15 @@
-# TODO: write the script
+# Credit Card
+echo "\n====== SCANNING FOR CREDIT CARD NUMBERS ======"
+grep -rE --color=always '(\b[0-9]{4}[- ]?){3}[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "========= CREDIT CARD SCAN COMPLETE =========="
+
+# Social Security
+echo "\n==== SCANNING FOR SOCIAL SECURITY NUMBERS ===="
+grep -rE --color=always '\b[0-9]{3}-[0-9]{2}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+echo "======= SOCIAL SECURITY SCAN COMPLETE ========"
+
+# Phone Numbers
+echo "\n========= SCANNING FOR PHONE NUMBERS ========="
+grep -rE --color=always '\b[0-9]{3}-[0-9]{3}-[0-9]{4}\b' . --exclude-dir={.git} --line-number
+grep -rE --color=always '\([0-9]{3}\) [0-9]{3}-[0-9]{4}' . --exclude-dir={.git} --line-number
+echo "========= PHONE NUMBER SCAN COMPLETE ========="
